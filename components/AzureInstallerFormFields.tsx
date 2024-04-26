9f7eaf1df2e5edf8014add41f3e3522c1358023f
Nat Hamilton <info@nathamilton.com>
2024-04-26 11:22:34 -0400

    feat: add support for passing field values via url search params
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- components/AzureInstallerFormFields.tsx
+++ components/AzureInstallerFormFields.tsx
@@ -1,12 +1,19 @@
 import React, { type FC } from "react";
 
-export const AzureInstallerFormFields: FC = () => {
+export const AzureInstallerFormFields: FC<{
+  searchParams?: Record<string, string>;
+}> = ({ searchParams = {} }) => {
   return (
     <>
       <label className="flex flex-col flex-auto gap-2">
         <span className="text-sm font-semibold">Azure location</span>
         <input
           className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+          defaultValue={
+            Object.hasOwn(searchParams, "location")
+              ? searchParams?.location
+              : ""
+          }
           name="location"
           type="text"
           required
@@ -14,39 +21,65 @@ export const AzureInstallerFormFields: FC = () => {
       </label>
 
       <label className="flex flex-col flex-auto gap-2">
-        <span className="text-sm font-semibold">Azure service principal app ID</span>
+        <span className="text-sm font-semibold">
+          Azure service principal app ID
+        </span>
         <input
           className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+          defaultValue={
+            Object.hasOwn(searchParams, "service_principal_app_id")
+              ? searchParams?.service_principal_app_id
+              : ""
+          }
           name="service_principal_app_id"
           type="text"
           required
         />
       </label>
 
-       <label className="flex flex-col flex-auto gap-2">
-        <span className="text-sm font-semibold">Azure service principal password</span>
+      <label className="flex flex-col flex-auto gap-2">
+        <span className="text-sm font-semibold">
+          Azure service principal password
+        </span>
         <input
           className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+          defaultValue={
+            Object.hasOwn(searchParams, "service_principal_password")
+              ? searchParams?.service_principal_password
+              : ""
+          }
           name="service_principal_password"
           type="password"
           required
         />
-       </label>
+      </label>
 
-        <label className="flex flex-col flex-auto gap-2">
+      <label className="flex flex-col flex-auto gap-2">
         <span className="text-sm font-semibold">Azure subscription ID</span>
         <input
           className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+          defaultValue={
+            Object.hasOwn(searchParams, "subscription_id")
+              ? searchParams?.subscription_id
+              : ""
+          }
           name="subscription_id"
           type="text"
           required
         />
-        </label>
+      </label>
 
-         <label className="flex flex-col flex-auto gap-2">
-        <span className="text-sm font-semibold">Azure subscription tenant ID</span>
+      <label className="flex flex-col flex-auto gap-2">
+        <span className="text-sm font-semibold">
+          Azure subscription tenant ID
+        </span>
         <input
           className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+          defaultValue={
+            Object.hasOwn(searchParams, "subscription_tenant_id")
+              ? searchParams?.subscription_tenant_id
+              : ""
+          }
           name="subscription_tenant_id"
           type="text"
           required
