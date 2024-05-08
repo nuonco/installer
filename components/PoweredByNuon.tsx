6431603c08aef66bf75a977e790438ab08effc9c
Jordan Acosta <jordan@nuon.co>
2024-05-08 08:30:30 -0700

    feat: add "Powered by Nuon" to footer (#4)

---
+++ components/PoweredByNuon.tsx
@@ -0,0 +1,16 @@
+import React, {type FC} from "react";
+import {Link} from "@/components";
+import {NuonLogotype} from "@/components/NuonLogotype";
+
+export const PoweredByNuon: FC = ({
+}) => {
+  return (
+    <span className="text-xs">
+      Powered by <Link href="https://www.nuon.co/" className="text-xs" target="_blank" rel="noreferrer" >
+        <NuonLogotype
+          className="inline w-auto h-6 ml-1 -mt-1.5 fill-black dark:fill-white"
+        />
+      </Link>
+    </span>
+  );
+};
