095fdf68e5f4713b340835ea111fde488d968d2e
Jordan Acosta <jordan@nuon.co>
2024-05-20 14:55:22 -0700

    feat: refactor theme file to make custom theming easier (#18)
    Update the theme file so it exports the entire Tailwind `theme` object.
    This will make it easier to drop in a custom theme if a vendor has one.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,20 +9,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/react-tailwindcss-select/dist/index.esm.js",
   ],
-  darkMode: theme.darkMode as Config["darkMode"],
   safelist: [
     {
       pattern: /bg-(red|green|yellow)-(500|600)/,
     },
   ],
-  theme: {
-    extend: {
-      colors: {
-        white: theme.white,
-        black: theme.black,
-        primary: theme.primary,
-      },
-    },
-  },
+  darkMode: theme.darkMode as Config["darkMode"],
+  theme: theme.theme,
 };
 export default config;
