1525e782fcc90090449f6995f8e6e47d6e948a67
Jordan Acosta <jordanjacosta@gmail.com>
2024-05-29 10:39:47 -0700

    feat: add basic styles

---
+++ theme.ts
@@ -0,0 +1,17 @@
+const colors = require("tailwindcss/colors");
+
+export default {
+  darkMode: "selector",
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        primary: colors.orange,
+        "page-background": "#eff5f9",
+        "card-border": "#266e85",
+        "card-background": "#ffffff",
+      },
+    },
+  },
+};
