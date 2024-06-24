c6d2a11f08d73700cbd85cde799ecb3d3d6aa965
Jordan Acosta <jordanjacosta@gmail.com>
2024-06-24 14:05:34 -0700

    fix: selector uses primary theme color

--- components/Select.tsx
+++ components/Select.tsx
@@ -11,8 +11,18 @@ interface Option {
 }
 
 const classNames = {
+  menuButton: ({ isDisabled }) =>
+    `flex text-sm text-gray-500 border border-gray-300 rounded shadow-sm transition-all duration-300 focus:outline-none ${
+      isDisabled
+        ? "bg-gray-200"
+        : "bg-white hover:border-gray-400 focus:border-primary-500 focus:ring focus:ring-primary-500/20"
+    }`,
   listItem: ({ isSelected }) =>
-    `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${isSelected ? `text-white bg-primary-500` : `text-gray-500 hover:bg-primary-200 hover:text-primary-600`}`,
+    `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${
+      isSelected
+        ? `text-white bg-primary-500`
+        : `text-gray-500 hover:bg-primary-200 hover:text-primary-600`
+    }`,
 };
 
 export const Select: FC<{
