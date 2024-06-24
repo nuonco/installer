8c16c5a0cfaeda2c0babe58606bc719760bd8f8a
Jordan Acosta <jordanjacosta@gmail.com>
2024-06-24 15:00:02 -0700

    feat: fully configurable card theme

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ export const Card: FC<{ children?: React.ReactNode; className?: string }> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col gap-2 rounded bg-card-background dark:bg-white items-start border border-card-border shadow shadow-card-border ${className}`}
+      className={`flex flex-col gap-2 rounded bg-card-background dark:bg-white items-start [border:_theme(borderWidth.card-border)_solid_theme(colors.card-border)] shadow-card-shadow shadow-card-color ${className}`}
     >
       {children}
     </div>
