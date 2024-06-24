b993f0ccd8719ca8ce896920392b473148e520a6
Jordan Acosta <jordan@nuon.co>
2024-06-24 15:39:27 -0700

    feat card and button theming (#33)
    * feat: fully configurable card theme
    
    * refactor: remove old Button components
    
    * feat: fully configurable button theme

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ export const Card: FC<{ children?: React.ReactNode; className?: string }> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col gap-2 rounded bg-card-background dark:bg-white items-start border border-card-border shadow shadow-card-border ${className}`}
+      className={`flex flex-col gap-2 bg-card-background dark:bg-white items-start border-card-border-color border-card-border-width rounded-card-radius shadow-card-shadow shadow-card-color ${className}`}
     >
       {children}
     </div>
