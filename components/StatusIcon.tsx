ca3d8b99b0af5a796a98e7b080dcabc6901f7dba
Jordan Acosta <jordan@nuon.co>
2024-07-01 14:33:34 -0700

    ci: set up vitest (#41)
    Trying out Vitest to start adding test coverage. This PR adds vitest,
    scripts for running and watching tests, a simple example test, and adds
    a Test step to ci workflow.

--- components/StatusIcon.tsx
+++ components/StatusIcon.tsx
@@ -9,6 +9,8 @@ export const StatusIcon: FC<{ status: string; className?: string }> = ({
     clr = "green-600";
   } else if (status === "error") {
     clr = "red-500";
+  } else if (status === "") {
+    clr = "slate-300";
   }
 
   return <span className={`text-${clr} ${className}`}>⏺</span>;
