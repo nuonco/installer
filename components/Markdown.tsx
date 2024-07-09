4647dc9db1162772c5c381978f542d4125f0b7c0
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-09 13:34:11 -0500

    feat(markdown): open markdown links in new tabs (#45)
    links in user-provided markdown content should be opened in a new tab.
    we achieve this by creating an extension for the showdown markdown rendering
    library, `targetlinks`.
    
    Source: https://github.com/showdownjs/showdown/issues/337
    
    Signed-off-by: fidiego <sayheytofred@gmail.com>

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,5 +1,18 @@
 import showdown from "showdown";
-const markdown = new showdown.Converter();
+
+// load extension to add target="_blank" to links
+showdown.extension("targetlink", () => {
+  return [
+    {
+      type: "html",
+      regex: /(<a [^>]+?)(>.*<\/a>)/g,
+      replace: '$1 target="_blank"$2',
+    },
+  ];
+});
+
+// instantiate converter
+const markdown = new showdown.Converter({ extensions: ["targetlink"] });
 
 export const Markdown = ({ content = "" }) => (
   <div
