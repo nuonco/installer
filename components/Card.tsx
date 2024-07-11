b9b0e55e92a74c34d183e5680e2ab5e8b20e4e2a
fidiego <sayheytofred@gmail.com>
2024-07-11 16:25:25 -0500

    feat: handle dark theme in cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,14 @@ export const Card: FC<{ children?: React.ReactNode; className?: string }> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col gap-2 bg-card-background items-start border-card-border-color border-card-border-width rounded-card-radius shadow-card-shadow shadow-card-color ${className}`}
+      className={`
+        flex flex-col gap-2  items-start
+        bg-card-background dark:bg-card-background-dark
+        border-card-border-color dark:border-card-border-color-dark
+        shadow-card-shadow
+        shadow-card-color dark:shadow-card-color-dark
+        border-card-border-width
+        rounded-card-radius ${className}`}
     >
       {children}
     </div>
