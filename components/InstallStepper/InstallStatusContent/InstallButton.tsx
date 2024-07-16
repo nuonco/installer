f78a9d42c291a3aa3a9b328e0cc887fed0b768e9
fidiego <sayheytofred@gmail.com>
2024-07-16 12:37:21 -0500

    feat: add card drop shadows back and in theme

--- components/InstallStepper/InstallStatusContent/InstallButton.tsx
+++ components/InstallStepper/InstallStatusContent/InstallButton.tsx
@@ -15,7 +15,12 @@ export const InstallButton = ({ install }) => {
     <Button
       loading={loading}
       type="submit"
-      className="text-sm text-button-text-color bg-button-bg-color hover:bg-button-bg-hover focus:bg-button-bg-focus active:bg-button-bg-active shadow-button-shadow !border-button-border-color border-button-border-width rounded-button-radius px-4 py-1.5"
+      className={`
+          text-sm text-button-text-color
+          bg-button-bg-color hover:bg-button-bg-hover focus:bg-button-bg-focus active:bg-button-bg-active
+          !shadow-button-shadow !dark:shadow-button-shadow-dark
+          !border-button-border-color border-button-border-width rounded-button-radius
+          px-4 py-1.5 mr-1`}
     >
       {label}
     </Button>
