77bdaedad69570e94db5edaf31f1cfebba613c77
fidiego <sayheytofred@gmail.com>
2025-02-04 14:17:51 -0600

    feat: install status: interpolate sandbox outputs
    we were previously only interpolating install inputs

--- components/InstallStepper/InstallStatusContent/index.tsx
+++ components/InstallStepper/InstallStatusContent/index.tsx
@@ -1,10 +1,27 @@
 import { Accordion, AccordionHeader, AccordionBody } from "../../Accordion";
 import { InstallStatus } from "./InstallStatus";
-import { StatusIcon } from "@/components";
 import { InstallButton } from "./InstallButton";
 import { Markdown } from "@/components/Markdown";
 
-const interpolateInputsAndInstallId = (install_id, inputs: {}, text) => {
+function flattenSandboxOutputs(obj: object, suffix: string, ans: object) {
+  for (var x in obj) {
+    var key: string;
+    if (suffix != "") key = suffix + "." + x;
+    else key = x;
+    if (typeof obj[x] === "object") {
+      flattenSandboxOutputs(obj[x], key, ans);
+    } else {
+      ans[key] = obj[x];
+    }
+  }
+}
+
+const interpolateInputsAndInstallId = (
+  install_id: string,
+  inputs: {},
+  sandbox_outputs: {},
+  text: string,
+) => {
   let interpolated = text;
   Object.keys(inputs).forEach((key) => {
     let value = inputs[key];
@@ -24,6 +41,18 @@ const interpolateInputsAndInstallId = (install_id, inputs: {}, text) => {
     interpolated = interpolated.replaceAll(regex, install_id);
   }
 
+  // flatten the sandbox_output keys so we can use this simple regex matching
+  let flatSandboxOutputs = {};
+  flattenSandboxOutputs(sandbox_outputs, "", flatSandboxOutputs);
+  Object.keys(flatSandboxOutputs).forEach((key) => {
+    let value = flatSandboxOutputs[key];
+    let regex = new RegExp(
+      String.raw`\{\{[ ]{0,1}\.nuon\.install\.sandbox\.outputs\.${key}[ ]{0,1}\}\}`,
+      "g",
+    ); // https://regex101.com/r/U5iEei/1
+    interpolated = interpolated.replaceAll(regex, value);
+  });
+
   return interpolated;
 };
 
@@ -35,6 +64,7 @@ export const InstallStatusContent = ({
     status: "",
     status_description: "",
     install_inputs: [],
+    sandbox: { outputs: {} },
   },
   post_install_markdown = "",
 }) => {
@@ -46,6 +76,7 @@ export const InstallStatusContent = ({
   const post_install_markdown_with_inputs = interpolateInputsAndInstallId(
     install.id,
     install_input_values,
+    install.sandbox.outputs,
     post_install_markdown,
   );
   return (
